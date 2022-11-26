5fc63646b3ff75470cb4e3db29d22763fe792f88
Hany Afifi <hanyafifi@cloudypedia.com>
2022-11-26 18:17:54 +0200

    tweak

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import './Portfolio.css'
+import iconBehance from "../../img/portfolio-icon-behance.svg";
+import iconGithub from "../../img/portfolio-icon-github.svg";
+import iconLink from "../../img/portfolio-icon-link.svg";
+import iconMero from "../../img/portfolio-icon-mero.svg";
+import portfolioPic01 from "../../img/portfolio-pic-01.jpg";
+
 
 
 const Portfolio = () => {
@@ -11,16 +17,21 @@ const Portfolio = () => {
                     <div className="cards-wrapper">
                         <div className="card">
                             <div className="pic">
-                                <img src="" alt="" />
+                                <img src={portfolioPic01} alt="Project Al Khat Alaraby picture" />
                             </div>
                             <div className="text">
-                                h2
+                                <h2>Mobile App UX Research and Design</h2>
+                                <h4>Al Khat Alaraby</h4>
+                            </div>
+                            <div className="icons">
+                                <img src={iconGithub} alt="Project Al Khat Alaraby GitHub" />
+                                <img src={iconBehance} alt="Project Al Khat Alaraby Behance" />
+                                <img src={iconMero} alt="Project Al Khat Alaraby Mero" />
+                                <img src={iconLink} alt="Project Al Khat Alaraby link" />
                             </div>
                         </div>
                     </div>
                 </div>
-
-
             </div>
         </div>
     )
