83fe4a81195530ac97be10b4992f98ef2873ad96
Hany Afifi <hanyafifi@cloudypedia.com>
2022-11-26 16:51:59 +0200

    tweak and enter portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,23 +9,25 @@ const navbar = () => {
   };
 
   return (
-    <div className="container">
-      <div className="navbar-wrapper">
-        <div className="nav-branding">
-          <img src={logoDark} alt="hanyafifi.dev dark theme logo" />
-        </div>
-        <div className="nav-links">
-          <ul>
-            <li className="current">Home</li>
-            <li>Portfolio</li>
-            <li>Work</li>
-            <li>Testimonials</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-        <div className="nav-buttons">
-          <button onClick={onClick} className="btn-cv">Download CV</button>
-          <span className="btn-dark-light">Toggle</span>
+    <div className="navi">
+      <div className="container">
+        <div className="navbar-wrapper">
+          <div className="nav-branding">
+            <img src={logoDark} alt="hanyafifi.dev dark theme logo" />
+          </div>
+          <div className="nav-links">
+            <ul>
+              <li className="current">Home</li>
+              <li>Portfolio</li>
+              <li>Work</li>
+              <li>Testimonials</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="nav-buttons">
+            <button onClick={onClick} className="btn-cv">Download CV</button>
+            <span className="btn-dark-light">Toggle</span>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Portfolio.css'
+
+
+const Portfolio = () => {
+    return (
+        <div className='portfolio'>
+            <div className="container">
+                <div className="title">
+                    <h1>Portfolio</h1>
+                    <div className="cards-wrapper">
+                        <div className="card">
+                            <div className="pic">
+                                <img src="" alt="" />
+                            </div>
+                            <div className="text">
+                                h2
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default Portfolio
