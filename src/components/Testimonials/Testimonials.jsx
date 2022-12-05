830f90a1125d351d1a556a3e32be640fd17ffea7
Hany Afifi <hanyafifi@cloudypedia.com>
2022-12-05 22:18:20 +0200

    tweak

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Testimonials.css'
 
-
-
-
+import logoHuman from "../../img/logo-human.svg";
+import logoBordersNone from "../../img/logo-bordersnone.svg";
+import logoPerpetuum from "../../img/logo-perpetuum.svg";
+import logoCloudypedia from "../../img/logo-cloudypedia.svg";
 
 import arrowLeft from "../../img/arrow-left.svg";
 import arrowRight from "../../img/arrow-right.svg";
@@ -17,26 +18,28 @@ const Testimonials = () => {
                 <h2>Testimonials</h2>
             </div>
 
-            <div className="cards-wrapper">
-                <div className="card">
-                    <div className="pic">
-
+            <div className="wrapper">
+                <div className="testimonials-slider">
+                    <div className="card greyed">
+                        <img src={logoHuman} className="logo" alt="Human Interactive" />
                     </div>
-                    <div className="text">
-                        <h3>Al Khat Alaraby</h3>
-                        <h4>Mobile App UX Research and Design</h4>
-
-                        <div className="icons">
-
-                        </div>
+                    <div className="card active">
+                        <img src={logoBordersNone} className="logo" alt="Borders None" />
+                    </div>
+                    <div className="card greyed">
+                        <img src={logoPerpetuum} className="logo" alt="Perpetuum Mobile" />
+                    </div>
+                    <div className="card greyed">
+                        <img src={logoCloudypedia} className="logo" alt="Cloudypedia" />
                     </div>
                 </div>
-            </div>
 
 
-            <div className="arrows">
-                <img src={arrowLeft} className="arrow arrow-left" alt="arrow left scroll" />
-                <img src={arrowRight} className="arrow arrow-right" alt="arrow right scroll" />
+
+                <div className="arrows">
+                    <img src={arrowLeft} className="arrow arrow-left" alt="arrow left scroll" />
+                    <img src={arrowRight} className="arrow arrow-right" alt="arrow right scroll" />
+                </div>
             </div>
         </div>
     )
